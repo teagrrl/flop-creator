ea0394372a05b2085768a8334a2023087f895103
Lilac Chang <mail@teagrrl.dev>
2022-09-26 17:10:47 -0400

    filter out starter partner pokemon

--- data/pokemon.ts
+++ data/pokemon.ts
@@ -167,7 +167,8 @@ function getStatValue(model: PokemonModel, stat: SortType) {
 export function FormFilter(showMega?: boolean, showGmax?: boolean) {
     return (model: PokemonModel) => 
         true 
-        && !(model.name.endsWith("-totem") || model.name.includes("-totem-")) // ignore totem pokemon 
+        && !model.name.endsWith("-starter") // ignore partner pikachu and eevee
+        && !(model.name.endsWith("-totem") || model.name.includes("-totem-")) // ignore totem pokemon
         && !model.name.endsWith("-hisui") // ignore hisui pokemon until they are properly in a mainline game
         && !(!showMega && (model.name.endsWith("-mega") || model.name.includes("-mega-"))) // ignore mega-evolution with flag
         && !(!showGmax && model.name.endsWith("-gmax")) // ignore gigantamax with flag
